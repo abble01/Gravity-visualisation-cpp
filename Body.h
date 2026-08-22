e2a296b28432ee96c37a85eb5544b864296c3dea
abble <rm99.mesioye@gmail.com>
2026-08-22 23:09:39 +0100

    Build basic Body class

--- Body.h
+++ Body.h
@@ -1,12 +1,31 @@
 #pragma once
 
+#include <SFML/System/Vector2.hpp>
+
 class Body
 {
-private:
-	sf::Vector2f position;
-	sf::Vector2f velocity;
 
-	float mass;
-	float radius;
+private:
+	sf::Vector2f m_position;
+	sf::Vector2f m_velocity;
+
+	float m_mass;
+	float m_radius;
+
+public:
+	Body(
+		sf::Vector2f position,
+		sf::Vector2f velocity,
+		float mass,
+		float radius
+	)
+		:
+		m_position(position),
+		m_velocity(velocity),
+		m_mass(mass),
+		m_radius(radius)
+	{
+	}
+
 
 };
